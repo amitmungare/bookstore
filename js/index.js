7015c48a3761cd24354a984eed519908862966d8
amitmungare <amungare27@gmail.com>
2023-10-08 21:10:53 +0530

    js

---
+++ js/index.js
@@ -0,0 +1,62 @@
+
+function changeName(){
+    // window.alert("hello i am here");
+    document.getElementById("p1").innerHTML = "Hey";
+    document.getElementById("p1").style.fontSize="40px";
+    console.log("this is log");
+
+    
+
+    var a = 10;
+    a = "heyyyy";
+    a = true;
+
+    var _a = 50;
+    var _a = 590;
+
+    d = 20;
+
+    var Yogita;
+    var yogita = 13;
+    var YOGITA  =14;
+
+    let s = "string";
+    let s1 = "not string";
+
+    const PI = 3.14;
+
+    const AMIT = "Amit Mungare"
+
+    var x = 11;
+    x = null;
+
+    var z = {
+        name:"amit",
+        rollNo:12,
+        pass: true,
+        sub:{
+            hindi:24,
+            maths:98,
+            math:[2, 3, 4]
+        }
+    }
+
+    var ss = [1,2,3];
+
+    var q = [12, "amitt", {name:"amit", age:90}, true]
+
+    const date = new Date("2022-03-25");
+
+
+    document.getElementById("myptag").innerHTML = typeof ss;
+
+    bbb();
+}
+
+function aaa(){
+    let b1 = 20;
+}
+
+function bbb(){
+    console.log(b1);
+}
