9c90b6ac9e7af1cda5c1dd5aa8d44cbe8e1b4939
amitmungare <amungare27@gmail.com>
2023-10-14 12:46:38 +0530

    js

--- js/index.js
+++ js/index.js
@@ -1,62 +1,154 @@
 
-function changeName(){
-    // window.alert("hello i am here");
-    document.getElementById("p1").innerHTML = "Hey";
-    document.getElementById("p1").style.fontSize="40px";
-    console.log("this is log");
-
-    
-
-    var a = 10;
-    a = "heyyyy";
-    a = true;
-
-    var _a = 50;
-    var _a = 590;
-
-    d = 20;
-
-    var Yogita;
-    var yogita = 13;
-    var YOGITA  =14;
-
-    let s = "string";
-    let s1 = "not string";
-
-    const PI = 3.14;
-
-    const AMIT = "Amit Mungare"
-
-    var x = 11;
-    x = null;
-
-    var z = {
-        name:"amit",
-        rollNo:12,
-        pass: true,
-        sub:{
-            hindi:24,
-            maths:98,
-            math:[2, 3, 4]
-        }
-    }
-
-    var ss = [1,2,3];
-
-    var q = [12, "amitt", {name:"amit", age:90}, true]
-
-    const date = new Date("2022-03-25");
+// function changeName(){
+//     // window.alert("hello i am here");
+//     document.getElementById("p1").innerHTML = "Hey";
+//     document.getElementById("p1").style.fontSize="40px";
+//     // console.log("this is log");
 
 
-    document.getElementById("myptag").innerHTML = typeof ss;
+//     var a = 10;
+//     a = "heyyyy";
+//     a = true;
 
-    bbb();
-}
+//     var _a = 50;
+//     var _a = 590;
 
-function aaa(){
-    let b1 = 20;
-}
+//     d = 20;
 
-function bbb(){
-    console.log(b1);
-}
+//     var Yogita;
+//     var yogita = 13;
+//     var YOGITA  =14;
+
+//     let s = "string";
+//     let s1 = "not string";
+
+//     const PI = 3.14;
+
+//     const AMIT = "Amit Mungare"
+
+//     var x = 11;
+//     x = null;
+
+//     // object 
+//     var z = {
+//         name:"amit",
+//         rollNo:12,
+//         pass: true,
+//         sub:{
+//             hindi:24,
+//             maths:98,
+//             math:[2, 3, 4]
+//         }
+//     }
+
+//     // Array
+//     var ss = [1,2,3];
+
+//     var q = [12, "amitt", {name:"amit", age:90}, true]
+
+//     // date
+//     const date = new Date("2022-03-25");
+
+
+//     document.getElementById("myptag").innerHTML = typeof ss;
+
+//     bbb();
+
+//     var zzz = 10;
+//     var zzz = 20;
+
+//     let $hhh = 20;
+
+//     // console.log(_hhh1)
+//     let _hhh1 = 40;
+
+//     // console.log(typeof "abc");
+//     // console.log(typeof 1);
+
+//     var a = 10*20;
+
+//     var b = myFun(10, 20);
+
+//     console.log(b);
+
+// }
+
+// function myFun(x, y){
+//     var c =  x * y;
+//     return c;
+// }
+
+
+
+
+// function aaa(){
+//     let b1 = 20;
+// }
+
+// function bbb(){
+//     var b1;
+//     // console.log(b1);
+// }
+
+// var b = myFun(10, 40);
+
+// function myFun(x, y){
+//     var c =  x * y;
+//     return c;
+// }
+
+
+// const person = {
+//     firstName:"John", 
+//     lastName:"Doe", 
+//     age:50, 
+//     eyeColor:"blue"
+// };
+
+// const person1 = {
+//     firstName: "John",
+//     lastName : "Doe",
+//     id   	: 5566,
+//     fullName : function() {
+//       return this.firstName ;
+//     }
+//   };
+  
+  
+//   console.log(person1.fullName)
+
+
+//   function getName(){
+//     document.getElementById("p2_id").innerHTML = Math.random()*100 ;
+//   }
+
+//   function colorRed(){
+//     document.getElementById("b1").style.backgroundColor = "red";
+//   }
+
+//   function colorGreen(){
+//     document.getElementById("b1").style.backgroundColor = "green";
+//   }
+
+// var stringname = "my string name is yogita and it is very good but lazy "
+
+
+// let firstName = "amit";
+// let lastName = "mungare";
+
+// let text = `${firstName} ${lastName} is my name`;
+
+// var x = Number.MAX_SAFE_INTEGER;
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.sort();
+// fruits.reverse();
+
+
+// const points = [40, 100, 1, 5, 25, 10];
+// points.sort(function(a, b){return a - b});
+// points.reverse()
+
+
+document.getElementById("test").innerHTML = "" ;
