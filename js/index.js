1d5021ecc5d647e661b4992ac07ddb6f8259e74a
amitmungare <amungare27@gmail.com>
2023-10-25 18:04:35 +0530

    pattern

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 
+
+
 // function changeName(){
 //     // window.alert("hello i am here");
 //     document.getElementById("p1").innerHTML = "Hey";
@@ -146,9 +148,309 @@
 // fruits.reverse();
 
 
-// const points = [40, 100, 1, 5, 25, 10];
+const points = [40, 100, 1, 5, 25, 10];
 // points.sort(function(a, b){return a - b});
 // points.reverse()
 
+// points.forEach(    //for(int i=0; i<10; i++)
+//     function myfun(num){
+//         console.log(num);
+//     }
+// )
 
-document.getElementById("test").innerHTML = "" ;
+// points.forEach((num)=>{
+//     console.log(num);
+// })
+
+
+// const numbers = [45, 4, 9, 16, 25];
+// numbers.forEach(myFunction);
+
+// function myFunction(value) {
+//     console.log(value);
+// }
+
+// const num = myfunc();
+
+// function myfunc(){
+//     return 20;
+// }
+
+
+// const myfunc = () => {
+//     return 20;
+// };
+
+// const num = myfunc();
+
+
+// const numbers1 = [45, 4, 9, 16, 25];
+
+// for(var i=0; i<numbers1.length; i++){
+//     console.log(numbers1[i]);
+// }
+
+// const numbers2 = numbers1.map(myFunction);
+
+// function myFunction(value) {
+//   return value * 2;
+// }
+
+// const myArr = [1, 2, 3, 4, 5, 6];
+// const newArr = myArr.flatMap((x) => x * 2);
+
+// const arr = [1, 2, 3, 4, 5, 6];
+
+// for(var i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// arr.forEach(function Arr(num){
+//     console.log(num)
+// })
+
+// arr.map(function Arr(num){
+//     console.log(num)
+// })
+
+// arr.flatMap((num)=>{
+//     console.log(num)
+// })
+
+// date
+// const d = new Date();
+
+
+// const mathNum = Math.random()*100;
+
+// const mathNum = Math.pow(8, 4);  //8*8*8*8
+
+// const mathNum = Math.min(40, 10);
+
+// var age = 13;
+// var text = "u can drink";
+// if(age<18)text ="u cannot drink"
+
+// var age = 79;
+// var text = "u can drink";
+// if(age<18 || age>60)text ="u cannot drink";
+
+// var num =0;
+// var text = "";
+
+// if(num>0){
+//     text = "pos"
+// }else if(num==0){
+//     text='zero'
+// }else{
+//     text = "nev"
+// }
+
+
+// let day;
+// switch (new Date().getDay()) {
+//     case 0:
+//       day = "Sunday";
+//       break;
+//     case 1:
+//       day = "Monday";
+//       break;
+//     case 2:
+//        day = "Tuesday";
+//       break;
+//     case 3:
+//       day = "Wednesday";
+//       break;
+//     case 4:
+//       day = "Thursday";
+//       break;
+//     case 5:
+//       day = "Friday";
+//       break;
+//     case 6:
+//       day = "Saturday";
+//   }
+
+
+// const arr = [100, 200, 300, 400, 500, 600];
+
+// for(var i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// for(var i in arr){
+//     console.log(arr[i]);
+// }
+
+// for(var i of arr){
+//     console.log(i);
+// }
+
+// const person = {
+//     fname:"amit", 
+//     lname:"mun", 
+//     age:25
+// };
+
+// var arr1 = ["abc", "xyz", "pqr"];
+
+// for(var x of arr1){
+//     console.log(x);
+// }
+
+
+// let text = "";
+// for (let x in person) {
+//   text += person[x]+" ";
+// }
+// console.log(text)
+
+
+// const numbers = [45, 4, 9, 16, 25];
+
+// let txt = "";
+// numbers.forEach(myFunction);
+
+// function myFunction(value) {
+//   txt += value+", ";
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     if (i === 3){ 
+//         continue; 
+//     }
+//   console.log(i)
+// }
+
+
+// const arr = [45, 4, 9];
+
+// var i=0;
+// while(i<arr.length){
+//   console.log(arr[i]);
+//   i++;
+// }
+
+// var i=0;
+// do{
+//   console.log(arr[i]);
+//   i++;
+// }while(i<arr.length)
+
+// var arr = [12, 23, 24, 36 ,79];
+
+// for(var i=0; i<arr.length; i++){
+//   if(arr[i]==24){
+//     break;
+//   }
+//   console.log(arr[i]);
+// }
+
+// var arr = [12, 23, 24, 36 ,79];
+
+// for(var i=0; i<arr.length; i++){
+//   if(arr[i]==24){
+//     continue;
+//   }
+//   console.log(arr[i]);
+// }
+
+// const name = "yogita";
+// for (const x of name) {
+//   console.log(x)
+// }
+
+// const myset = new Set([1,2,3,1,3,6]);
+
+// myset.add(45);
+// myset.add(56);
+// myset.add(45);
+
+// let text = "";
+
+// myset.forEach(function(value){
+//   text+=value+" ";
+// })
+
+
+// const fruitsMap = new Map();
+
+// fruitsMap.set("apple", 200);
+// fruitsMap.set("bananas", 100);
+// fruitsMap.set("oranges", 150);
+// fruitsMap.set("apple", 300);
+// fruitsMap.set("apple", 100);
+
+// Number("3.14")
+
+// const cars = [10,20,30];
+// let text = "";
+// for (let x of cars) {
+//   text += x+" ";
+// }
+
+// try{
+//     var i=12/0;
+// }catch(error){
+//     i=0;
+// }
+
+// {
+
+//     let i="aaaa";
+
+// }
+
+// function myfun(){
+//     var aaa="aaaaaaa";
+
+// }
+
+// myfun()
+
+
+// let c = 10;
+
+// var firstName = "yogita"
+
+
+// const person = {
+
+//     firstName: "amit",
+//     lastName : "abc",
+//     id   	: 5566,
+//     fullName : function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+    
+//   };
+  
+// var x=10;
+// var z=20;
+
+// hello = (x, z) => {
+//     return "Hello World! " + x+z;
+// }
+  
+
+// class Car {
+//     constructor(name, year) {
+//       this.name = name;
+//       this.year = year;
+//     }
+// }
+// const myCar1 = new Car("Ford", 2014);  
+// const myCar2 = new Car("bmw", 2024);  
+
+
+var a = [1,2,3];
+var b = a;
+a.push(4);
+
+console.log(a);
+console.log(b);
+
+
+document.getElementById("test").innerHTML = myCar1.year;
+
+
